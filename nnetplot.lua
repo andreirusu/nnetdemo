ec3f9990c949a6f90b0a92b9eda65e6b9cb1a6d3
Andrei Rusu <andreirusu@deepmind.com>
2013-04-02 21:49:39 +0100

    Added plotting script.

---
+++ nnetplot.lua
@@ -0,0 +1,38 @@
+require 'torch'
+require 'nn'
+require 'nnet'
+require 'sys'
+require 'optim'
+
+
+
+local function main()
+    local options = nnet.parse_arg(arg)
+    
+    options = nnet.set_options(options)
+  
+    nnet.init_experiment(options)
+
+    local samples = nnet.get_data(options)
+   
+
+    local mlp = nnet.get_model(options) 
+    
+    local funs = {  function(x)  
+                        return torch.abs(x) * torch.sin(x) 
+                    end, 
+                    function(x) 
+                        return mlp:forward(torch.Tensor(1):fill(x)):squeeze() 
+                    end
+                }
+
+
+    local mlp_loss = nnet.eval_net(samples, funs, mlp, options)
+       
+    nnet.plot(samples, funs, options)
+    print(string.format('Current MLP: %.4f', mlp_loss))
+    
+end
+
+main()
+    
