6514fee1025fcab874fa572bdd61c9e1068fa4b2
Andrei Rusu <andreirusu@deepmind.com>
2013-04-02 13:06:54 +0100

    Exported functions to nnet package.

---
+++ nnet.lua
@@ -0,0 +1,78 @@
+require 'torch'
+require 'nn'
+
+nnet = {}
+
+function nnet.set_options()
+    local options = {}
+    options.input = {}
+
+    options.input.size  = 1000
+    options.input.min   = -5
+    options.input.max   = 5
+    options.input.cols  = 1
+
+    options.seed = os.time()
+
+    options.n_units = {options.input.cols, 100,  0, 1}
+
+    torch.manualSeed(options.seed)
+
+    return options
+end
+
+function nnet.get_net(options)
+    local mlp = nn.Sequential()
+
+    local init_weight = function(x) 
+        return torch.randn(1):mul(1):squeeze()
+    end
+
+    local init_bias = function(x) 
+        return torch.randn(1):mul(0.1):squeeze()
+    end
+
+    local n_old 
+    for _, v in pairs(options.n_units) do
+        if v ~= 0 then
+            if n_old then 
+                local lin = nn.Linear(n_old, v)
+                lin.weight:apply(init_weight)
+                lin.bias:apply(init_bias)
+                mlp:add(lin)
+            end
+            n_old = v
+        else 
+            mlp:add(nn.Tanh())
+        end
+    end
+
+    
+
+    
+    return mlp
+end
+
+
+function nnet.plot(samples, funs, options)
+    if type(funs) == 'function' then
+        funs = {funs}
+    end
+
+    local tab = {}
+    for _,fun in pairs(funs) do
+        table.insert(tab, {samples, samples:clone():apply(fun), '+-'})
+    end
+    gnuplot.plot(tab)
+end
+
+function nnet.get_data(options)
+    return (torch.rand(options.input.size) * (options.input.max - options.input.min) + options.input.min):sort()
+end
+
+function nnet.eval_net(samples, funs, options)
+    return torch.mean(torch.abs(samples:clone():apply(funs[1]):add(-samples:clone():apply(funs[2])))) -- L1
+    --return torch.mean(torch.pow(samples:clone():apply(funs[1]):add(-samples:clone():apply(funs[2])),2)) -- L2
+end
+
+
--- nnetdemo.lua
+++ nnetdemo.lua
@@ -1,90 +1,18 @@
 require 'torch'
 require 'nn'
-
-
-local function set_options()
-    local options = {}
-    options.input = {}
-
-    options.input.size  = 1000
-    options.input.min   = -5
-    options.input.max   = 5
-    options.input.cols  = 1
-
-    options.seed = os.time()
-
-    options.n_units = {options.input.cols, 100,  0, 1}
-
-    torch.manualSeed(options.seed)
-
-    return options
-end
-
-local function get_net(options)
-    local mlp = nn.Sequential()
-
-    local init_weight = function(x) 
-        return torch.randn(1):mul(1):squeeze()
-    end
-
-    local init_bias = function(x) 
-        return torch.randn(1):mul(0.1):squeeze()
-    end
-
-    local n_old 
-    for _, v in pairs(options.n_units) do
-        if v ~= 0 then
-            if n_old then 
-                local lin = nn.Linear(n_old, v)
-                lin.weight:apply(init_weight)
-                lin.bias:apply(init_bias)
-                mlp:add(lin)
-            end
-            n_old = v
-        else 
-            mlp:add(nn.Tanh())
-        end
-    end
-
-    
-
-    
-    return mlp
-end
-
-
-local function plot(samples, funs, options)
-    if type(funs) == 'function' then
-        funs = {funs}
-    end
-
-    local tab = {}
-    for _,fun in pairs(funs) do
-        table.insert(tab, {samples, samples:clone():apply(fun), '+-'})
-    end
-    gnuplot.plot(tab)
-end
-
-local function get_data(options)
-    return (torch.rand(options.input.size) * (options.input.max - options.input.min) + options.input.min):sort()
-end
-
-local function eval_net(samples, funs, options)
-    return torch.mean(torch.abs(samples:clone():apply(funs[1]):add(-samples:clone():apply(funs[2])))) -- L1
-    --return torch.mean(torch.pow(samples:clone():apply(funs[1]):add(-samples:clone():apply(funs[2])),2)) -- L2
-end
+require 'nnet'
 
 local function main()
-    local options = set_options()
+    local options = nnet.set_options()
    
-    local samples = get_data(options)
+    local samples = nnet.get_data(options)
    
 
     local best_mlp 
     local best_mlp_loss = math.huge
 
     while true do
-        local mlp = get_net(options)  
+        local mlp = nnet.get_net(options)  
         --print(mlp)
 
         
@@ -96,7 +24,7 @@ local function main()
                         end
                     }
 
-        local mlp_loss = eval_net(samples, funs, mlp, options)
+        local mlp_loss = nnet.eval_net(samples, funs, mlp, options)
         if best_mlp_loss > mlp_loss then
             best_mlp = mlp
             best_mlp_loss = mlp_loss
@@ -110,7 +38,7 @@ local function main()
                             end)
         end
 
-        plot(samples, funs, options)
+        nnet.plot(samples, funs, options)
         --os.execute('sleep 2')
     end
 end
