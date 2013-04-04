9dee36dc7de5a544102f152c81109c7955ce0b3c
Andrei Rusu <andreirusu@deepmind.com>
2013-04-04 23:11:49 +0100

    Added NL plotting script, together with current objective function.

---
+++ nnetNLplot.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env torch
+require 'torch'
+require 'torch-env'
+require 'nn'
+require 'nnet'
+require 'sys'
+require 'optim'
+
+
+
+local function main()
+    local options = nnet.parse_arg(arg)
+    options = nnet.set_options(options)
+    nnet.init_experiment(options)
+    
+    local samples = nnet.get_data(options)
+    local mlp = nnet.get_model(options) 
+   
+    
+    nnet.updatePNLParameters(mlp, options) 
+
+    local funs = {  options.objectiveFunction, 
+                    function(x)
+                        return options.NL:forward(torch.Tensor(options.h1):fill(x))[1] 
+                    end
+                }
+
+    --local mlp_loss = nnet.eval_net(samples, funs, mlp, options)
+       
+    nnet.plot(samples, funs, options)
+    --print(string.format('Current MLP: %.4f', mlp_loss))
+    
+end
+
+main()
+    
