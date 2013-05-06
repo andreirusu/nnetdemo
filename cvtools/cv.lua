456b467ffdd7790a2d2086af69ef64b3f7c4cca9
Andrei A. Rusu <andreirusu@deepmind.com>
2013-05-06 07:01:37 +0100

    Functional k-fold cross validation splitting tool.

--- cvtools/cv.lua
+++ cvtools/cv.lua
@@ -15,9 +15,9 @@ function cv.parse_arg(arg, initNLparams)
     cmd:text('Cross-validation tool for splitting torch-datasets in folds which are saved in the current directory')
     cmd:text()
     cmd:text('Options:')
-    cmd:option('-dataset',      '',         'subdirectory to save/log experiments in')
-    cmd:option('-k',            4,                     'reload pretrained network')
-    cmd:option('-seed',         0,                      'fixed input seed for repeatable experiments')
+    cmd:option('-dataset',      '',         'saved TableDataset which will be split up')
+    cmd:option('-k',            4,          'number of folds')
+    cmd:option('-seed',         0,          'fixed input seed for repeatable experiments')
     
     cmd:text()
     return cmd:parse(arg)
@@ -34,18 +34,17 @@ function cv.wrapFunction(str)
     return loadstring('return '..str)()
 end
 
-function cv.getTableDatasets(options)
-    return {train=torch.load(paths.concat(options.dataset, 'train.th7')), 
-                test=torch.load(paths.concat(options.dataset, 'test.th7'))}
+function cv.getTableDataset(filepath)
+    local ret = torch.load(filepath) 
+    print(ret)
+    return ret
 end
 
 
 function cv.set_options(options)
     local options = options or {}
     assert(options.dataset, 'A dataset path must be specified!') 
-    assert(paths.dirp(options.dataset), '-dataset path must be a directory containing two valid th7 files: train.th7 & test.th7!')
-    assert(paths.filep(paths.concat(options.dataset, 'train.th7')), '-dataset path must be a directory containing a valid train.th7 file!')
-    assert(paths.filep(paths.concat(options.dataset, 'test.th7')), '-dataset path must be a directory containing a valid test.th7 file!')
+    assert(paths.filep(options.dataset), '-dataset path must be a valid TableDataset!')
     return options
 end
 
--- cvtools/cvsplit.lua
+++ cvtools/cvsplit.lua
@@ -8,6 +8,139 @@ require 'dataset/TableDataset'
 
 require 'cv'
 
+local function permute_rows(data, perm)
+    if not data then return nil end
+    local pdata = torch.Tensor(data:size())
+    if pdata:nDimension() == 1 then
+        for i = 1,data:size(1) do
+            pdata[perm[i]] = data[i]
+        end
+    else
+        for i = 1,data:size(1) do
+            pdata[perm[i]]:copy(data[i])
+        end
+    end
+    assert(torch.sum(torch.abs(torch.mean(data, 1) 
+                                - torch.mean(pdata, 1))) < 1e-10)
+    return pdata
+end
+
+function cv.KFolds(_ds, options) 
+    local ds = _ds.dataset
+    
+    if ds.cass then 
+        perm_class = torch.Tensor(ds.class:size())
+    end
+
+    local perm = torch.randperm(ds.data:size(1))
+    
+    local perm_data = permute_rows(ds.data, perm)
+    local perm_class = permute_rows(ds.class, perm)
+
+    local folds = {}
+    
+    assert(not paths.dirp('kfolds'))
+    local fold_size = math.abs(ds.data:size(1)/options.k)
+    local root_path = './kfolds/'
+    print(fold_size)
+    -- first fold
+    do
+        local i = 1
+        os.execute('mkdir -p '..root_path..i)
+        do
+            -- save test
+            local ds_table = {}
+            ds_table.data=perm_data:narrow(1, 1, fold_size):clone() 
+            if perm_class then ds_table.class=perm_class:narrow(1, 1, fold_size):clone() end 
+            local fold_ds = dataset.TableDataset(ds_table)
+            print(fold_ds)
+            torch.save(paths.concat(paths.concat(root_path, i), 'test.th7'), fold_ds)
+        end
+        
+        do
+            -- save train
+            local ds_table = {}
+            ds_table.data=perm_data:narrow(1, fold_size+1, perm_data:size(1) - fold_size):clone() 
+            if perm_class then 
+                ds_table.class=perm_class:narrow(1, fold_size+1, perm_class:size(1) - fold_size):clone() 
+            end 
+            local fold_ds = dataset.TableDataset(ds_table)
+            print(fold_ds)
+            torch.save(paths.concat(paths.concat(root_path, i), 'train.th7'), fold_ds)
+        end 
+    end
+    -- intermediate folds
+    for i=2,options.k-1 do
+        os.execute('mkdir -p '..root_path..i)
+        
+        do
+            -- save test
+            local ds_table = {}
+            ds_table.data=perm_data:narrow(1, (i-1)*fold_size+1, fold_size):clone() 
+            if perm_class then ds_table.class=perm_class:narrow(1, (i-1)*fold_size+1, fold_size):clone() end 
+            local fold_ds = dataset.TableDataset(ds_table)
+            print(fold_ds)
+            torch.save(paths.concat(paths.concat(root_path, i), 'test.th7'), fold_ds)
+        end
+
+        do
+            -- save train
+            local ds_table = {}
+            ds_table.data=torch.cat(perm_data:narrow(1, 1, (i-1)*fold_size),
+                                    perm_data:narrow(1, i*fold_size+1, perm_data:size(1) - i*fold_size), 1):clone() 
+            if perm_class then 
+                ds_table.class=torch.cat(perm_class:narrow(1, 1, (i-1)*fold_size),
+                                            perm_class:narrow(1, i*fold_size+1, perm_class:size(1) - i*fold_size), 1):clone() 
+            
+            end 
+            local fold_ds = dataset.TableDataset(ds_table)
+            print(fold_ds)
+            torch.save(paths.concat(paths.concat(root_path, i), 'train.th7'), fold_ds)
+        end 
+    end
+    -- last fold
+    do
+        local i = options.k
+        os.execute('mkdir -p '..root_path..i)
+        do
+            -- save test
+            local ds_table = {}
+            ds_table.data=perm_data:narrow(1, (i-1)*fold_size+1, perm_data:size(1) - (i-1)*fold_size):clone() 
+            if perm_class then ds_table.class=perm_class:narrow(1, (i-1)*fold_size+1, perm_class:size(1) - (i-1)*fold_size):clone() end 
+            local fold_ds = dataset.TableDataset(ds_table)
+            print(fold_ds)
+            torch.save(paths.concat(paths.concat(root_path, i), 'test.th7'), fold_ds)
+        end
+
+        do
+            -- save train
+            local ds_table = {}
+            ds_table.data=perm_data:narrow(1, 1, (i-1)*fold_size):clone() 
+            if perm_class then 
+                ds_table.class=perm_class:narrow(1, 1, (i-1)*fold_size):clone() 
+            end 
+            local fold_ds = dataset.TableDataset(ds_table)
+            print(fold_ds)
+            torch.save(paths.concat(paths.concat(root_path, i), 'train.th7'), fold_ds)
+        end 
+    end
+    -- test folds
+    for i=1,options.k do
+        print('Checking fold ', i)
+        local train_ds = torch.load(paths.concat(paths.concat(root_path, i), 'train.th7'))
+        local test_ds = torch.load(paths.concat(paths.concat(root_path, i), 'test.th7'))
+
+        print(test_ds)
+        print(train_ds)
+
+        assert(torch.std(torch.cat(train_ds.dataset.data, test_ds.dataset.data, 1)) 
+                                     - torch.std(perm_data) < 1e-10)
+        assert(torch.std(torch.cat(train_ds.dataset.class, test_ds.dataset.class, 1)) 
+                                     - torch.std(perm_class) < 1e-10)
+    end
+    
+    return folds
+end
 
 local function main()
     local options = cv.parse_arg(arg, true)
@@ -16,11 +149,9 @@ local function main()
   
     cv.init_experiment(options)
     
-    data = cv.getTableDatasets(options)
+    ds = cv.getTableDataset(options.dataset)
 
-    print(data.train)
-    print(data.test)
-     
+    cv.KFolds(ds, options) 
 end
 
 
