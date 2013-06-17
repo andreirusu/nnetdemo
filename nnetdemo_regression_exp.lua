943763720f05686019bd9b59e9117ae93efe1760
Andrei Rusu <andreirusu@deepmind.com>
2013-06-17 19:43:11 +0100

    Added experimental regression demo, where the new features are introduced and tested

---
+++ nnetdemo_regression_exp.lua
@@ -0,0 +1,122 @@
+#!/usr/bin/env torch
+require 'torch'
+require 'torch-env'
+require 'nn'
+require 'nnet_regression'
+require 'sys'
+require 'optim'
+require 'GaussianNoise'
+require 'Hebbian'
+require 'Deviation'
+
+
+
+
+function train_network(model, samples, config, options)
+    local criterion = nn.MSECriterion()
+    local parameters, gradParameters = model:getParameters()
+    local shuffle = torch.randperm(options.size)
+
+
+    local inputs = torch.Tensor(options.size, 1):copy(samples.x)
+    local targets = torch.Tensor(options.size, 1):copy(samples.y)
+
+    local feval = function(x) 
+        local f = 0
+        gradParameters:zero()
+
+        for t = 1,options.szMinibatch do
+            local i = shuffle[t]
+            local pred = model:forward(inputs[i])
+            loss = criterion:forward(pred, targets[i])
+            f = f + loss
+            local df_do = criterion:backward(pred, targets[i])
+            
+            model:backward(inputs[i], df_do)
+            
+        end
+        -- average parameters
+        gradParameters:div(options.size)
+
+        return f, gradParameters
+    end
+
+    optim.sgd(feval, parameters, config)
+end
+
+
+
+local function set_options(options)
+    local options = options or {}
+     
+    options.n_units = {options.cols, options.h1, 0,                     --  single layer ANN with 10 tanh units
+                                                --nnd.Hebbian(), 
+                                                nnd.GaussianNoise(),    --  normally distributed noisy tanh units
+                                                1,                      --  one linear output unit
+                                                nnd.Hebbian(), 
+                                                nnd.Deviation() }       --  back-propagated error is deviation from mean activity
+
+    options.nl = nnet.wrapFunction(options.nl)
+
+    return options
+end
+
+
+
+local function main()
+    local options = nnet.parse_arg(arg, true)
+    
+    options = set_options(options)
+  
+    nnet.init_experiment(options)
+
+    local samples = nnet.get_data(options)
+   
+
+    local best_mlp 
+    local best_mlp_loss = math.huge
+    local mlp
+    
+    local config = {learningRate        = options.learningRate,
+                    momentum            = options.momentum,
+                    weightDecay         = options.weightDecay,
+                    learningRateDecay   = options.learningRateDecay }
+    local epoch = 0 
+    while true do
+        mlp = mlp or nnet.get_model(options) 
+        
+        local funs = {  options.objectiveFunction, 
+                        function(x) 
+                            return mlp:forward(torch.Tensor(1):fill(x)):squeeze() 
+                        end
+                    }
+
+        train_network(mlp, {x=samples, y=samples:clone():apply(funs[1])}, config, options)
+
+        if best_mlp then
+            table.insert(funs,
+                            function(x) 
+                                return best_mlp:forward(torch.Tensor(1):fill(x)):squeeze() 
+                            end)
+        end
+
+
+
+        local mlp_loss = nnet.eval_net(samples, funs, mlp, options)
+        if best_mlp_loss > mlp_loss then
+            best_mlp = mlp:clone()
+            best_mlp_loss = mlp_loss
+        end
+        if epoch % options.saveEvery == 0 then 
+            nnet.save_network({network=best_mlp}, options)
+        end
+        if epoch % options.reportEvery == 0 then 
+            nnet.plot(samples, funs, options)
+            print(string.format('Epoch %3d: Best MLP: %.4f\tCurrent MLP: %.4f', epoch, best_mlp_loss, mlp_loss))
+        end
+        epoch = epoch + 1
+    end
+end
+
+main()
+    
